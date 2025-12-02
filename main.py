661dd63bcec50ad9635dbbb7c8a6c97bd38a3635
Abdullah <Almutairi.swe@gmail.com>
2025-12-02 23:08:22 +0300

    remove size check, upload files directly on detection

--- main.py
+++ main.py
@@ -7,33 +7,20 @@ import paramiko
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
-
 def on_created(event):
+    #When ever he detect a file he will upload
     filepath = event.src_path
-    print(f"Created: {filepath}")
+    print(f"Found: {filepath}")
 
-    #Now we need to make sure the file is done Downloading
-    last_size = -1
-    while True:
-        try:
-            current_size = os.path.getsize(filepath)
-        except:
-            time.sleep(2)
-            continue
-        if current_size == last_size:
-            break
-            last_size = current_size
-            time.sleep(2)
-
-    print("file finished. now uploading")
+    print("Now Uploading ..")
 
     #Uplaod now
 
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname = 'your ip',username = 'your username',password = 'password for the user',port=22)
+    ssh.connect(hostname = 'put your ip',username = 'put your username',password = 'the password for username',port=22)
     sftp_client = ssh.open_sftp() 
-    sftp_client.put(filepath, f"'/To/your/des/{os.path.basename(filepath)}")
+    sftp_client.put(filepath, f"/To/Your/server/dir/{os.path.basename(filepath)}")
     sftp_client.close()
     ssh.close()
     print("upload complete")
@@ -43,7 +30,7 @@ if __name__ == "__main__":
     # Calling the function
     event_handler.on_created = on_created
     #Path
-    path = "your/path"
+    path = "Your completed torrent file"
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
@@ -53,6 +40,6 @@ if __name__ == "__main__":
             time.sleep(1)
     finally:
         observer.stop()
-         print("Done")
-         observer.join()
+        print("Done")
+        observer.join()
 
